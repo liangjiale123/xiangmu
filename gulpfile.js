14698321deb7bb049f6f8d21143fbbb33836426e
liangjiale123 <592622846@qq.com>
2017-05-15 15:15:22 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+﻿/**
+ * Created by Administrator on 2017/5/10.
+ */
+var gulp = require("gulp");
+var less = require("less");
+var cleancss = require("gulp-clean-css");
+var plugins = require("gulp-load-plugins")();
+gulp.task("show",function () {
+    console.log("hello gulp");
+});
+gulp.task('myless',function () {
+    gulp.src('app/*.less')
+        .pipe(plugins.less())
+        .pipe(gulp.dest('app/'));
+});
+gulp.task("mywatch",function () {
+    gulp.watch("app/*.less",["myless"])
+});
