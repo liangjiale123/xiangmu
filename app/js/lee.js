0c31445d7fd6a17b4499518e4b04f89f5173f9ed
xujincheng <632618058.qq.com>
2017-05-17 10:11:15 +0800

    首页页面地图页面

--- app/js/lee.js
+++ app/js/lee.js
@@ -1,3 +1,43 @@
 /**
  * Created by Administrator on 2017/5/15.
  */
+$(function () {
+   $("#div01").click(function () {
+      $("#p1").css("color","#f36621");
+       $("#div01").css("border-bottom","1px solid #f36621");
+       $("#p2").css("color","black");
+       $("#div02").css("border-bottom","1px solid #9b9b9b");
+       $("#dianying").css("display","block");
+   });
+});
+
+$(function () {
+    $("#div02").click(function () {
+        $("#p2").css("color","#f36621");
+        $("#div02").css("border-bottom","1px solid #f36621");
+        $("#p1").css("color","black");
+        $("#div01").css("border-bottom","1px solid #9b9b9b");
+        $("#dianying").css("display","none");
+    });
+});
+$(function () {
+    $("#qq")[0].click(function () {
+            $("#kuozhan").css("display","block");
+        });
+});
+$(function () {
+    $("#qq")[0].click(function () {
+        $("#kuozhan").css("display","none");
+
+    });
+});
+$(function () {
+   window.onscroll=function () {
+     var top =scroll().top;
+       if(top>1000){
+           $("#qwe").css("display","block");}
+           else {
+               $("#qwe").css("display","none");
+           };
+       };
+});
